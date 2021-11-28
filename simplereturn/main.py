864fb43f5aabcec825e11441d280661a8bda7d05
Hizaguru <jukkajla@metropolia.fi>
2021-11-28 12:50:09 +0200

    building the methods.

---
+++ simplereturn/main.py
@@ -0,0 +1,42 @@
+import numpy as np
+from pandas_datareader import data as wb
+import matplotlib.pyplot as plt
+
+
+stockName = []
+
+def stockSimplereturn(stockName, beginningDay, to):
+
+    stock = wb.DataReader(stockName, data_source='yahoo', start= beginningDay, end=to)
+    #adjusted close Pt/Pt-1
+    stock['simple_return'] = (stock['Adj Close'] / stock['Adj Close'].shift(1)) - 1
+    #OPrints the plot, size 10x5.
+    stock['simple_return'].plot(figsize=(10,5))
+    plt.show()
+    #Average return per day.
+    avg_returns_day = stock['simple_return'].mean()
+    print(stockName + "s avarage returns per day is: " + str(round(avg_returns_day, 6) * 100) + '%')
+
+    avg_returns_a = stock['simple_return'].mean() * 250
+    print(stockName + "'s average return is: " + str(round(avg_returns_a, 4) * 100) + '%')
+
+#Calculate Log returns ln(Pt/Pt-1)
+def stock_log_return(stockName, beginningDay, to):
+    stock = wb.DataReader(stockName, data_source='yahoo', start=beginningDay, end=to)
+
+    stock['log_return'] = np.log(stock['Adj Close'] / stock['Adj Close'].shift(1))
+
+    stock['log_return'].plot(figsize=(10, 5))
+    plt.text(0.5,5, stockName + " log returns.")
+    plt.show()
+
+    log_returns_day = stock['log_return'].mean()
+    print(stockName + "s avarage log return per day is: " + str(round(log_returns_day, 6) * 100) + '%')
+
+    log_returns_a = stock['log_return'].mean() * 250
+    print(stockName + "'s average log return is: " + str(round(log_returns_a, 4) * 100) + '%')
+
+
+if __name__ == '__main__':
+    stockSimplereturn('ENJ-USD', '2020-1-1', '2021-1-10')
+    stock_log_return('ENJ-USD', '2020-1-1', '2021-1-10')
