c245c0968cb2d871d4dfeb30e036759e0fb99904
Hizaguru <jukkajla@metropolia.fi>
2021-11-29 07:15:02 +0200

    Calculates simple returns and draws the plot.

--- simplereturn/main.py
+++ simplereturn/main.py
@@ -1,9 +1,18 @@
 import numpy as np
+import pandas as pd
 from pandas_datareader import data as wb
 import matplotlib.pyplot as plt
 
 
-stockName = ['ENJ-EUR', 'BTC-EUR', 'ETH-EUR', 'MANA-EUR']
+tickers = ['ENJ-EUR', 'BTC-EUR', 'ETH-EUR', 'MANA-EUR']
+
+def portFolioOfSecurities(beginningDay):
+    data = pd.DataFrame()
+    for ticker in tickers:
+        data[ticker] = wb.DataReader(ticker, data_source='yahoo', start=beginningDay)['Close']
+    print(data.info())
+    print(data.head())
+    print(data.tail())
 
 def stockSimplereturn(stockName, beginningDay, to):
 
@@ -12,13 +21,14 @@ def stockSimplereturn(stockName, beginningDay, to):
     stock['simple_return'] = (stock['Adj Close'] / stock['Adj Close'].shift(1)) - 1
     #OPrints the plot, size 10x5.
     stock['simple_return'].plot(figsize=(10,5))
+    plt.title(stockName + "'s simple return")
     plt.show()
     #Average return per day.
-    avg_returns_day = stock['simple_return'].mean()
-    print(stockName + "s avarage returns per day is: " + str(round(avg_returns_day, 6) * 100) + '%')
+    dailyReturn = stock['simple_return'].mean()
+    print(stockName + "s avarage returns per day is: " + str(round(dailyReturn, 6) * 100) + '%')
 
-    avg_returns_a = stock['simple_return'].mean() * 250
-    print(stockName + "'s average return is: " + str(round(avg_returns_a, 4) * 100) + '%')
+    annual_return = stock['simple_return'].mean() * 250
+    print(stockName + "'s average return is: " + str(round(annual_return, 4) * 100) + '%')
 
 #Calculate Log returns ln(Pt/Pt-1)
 def stock_log_return(stockName, beginningDay, to):
@@ -27,19 +37,22 @@ def stock_log_return(stockName, beginningDay, to):
     stock['log_return'] = np.log(stock['Adj Close'] / stock['Adj Close'].shift(1))
 
     stock['log_return'].plot(figsize=(10, 5))
-    plt.text(0.5,5, stockName + " log returns.")
+    plt.title(stockName + "'s simple return")
     plt.show()
 
-    log_returns_day = stock['log_return'].mean()
-    print(stockName + "s avarage log return per day is: " + str(round(log_returns_day, 6) * 100) + '%')
+    daily_log_return = stock['log_return'].mean()
+    print(stockName + "s avarage log return per day is: " + str(round(daily_log_return, 6) * 100) + '%')
 
-    log_returns_a = stock['log_return'].mean() * 250
-    print(stockName + "'s average log return is: " + str(round(log_returns_a, 4) * 100) + '%')
+    annual_log_return = stock['log_return'].mean() * 250
+    print(stockName + "'s average log return is: " + str(round(annual_log_return, 4) * 100) + '%')
 
 
 if __name__ == '__main__':
-    for stock in stockName:
+    for stock in tickers:
         stockSimplereturn(stock, '2020-1-1', '2021-1-10')
+    #portFolioOfSecurities('2020-1-1')
+
+
 
 
 
